720099635ddc0e863cf97bc5b4a7292bdd3f8092
Tuoris <tuoris.dev@gmail.com>
2017-03-08 15:04:25 +0200

    Initial commmit

---
+++ setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+requires = [
+    'setuptools',
+]
+
+test_requires = requires + [
+    'webtest',
+    'python-coveralls',
+    'openprocurement.tender.belowthreshold',
+]
+
+docs_requires = requires + [
+    'sphinxcontrib-httpdomain',
+]
+
+databridge_requires = requires + [
+    'PyYAML',
+    'gevent',
+    'redis',
+    'LazyDB',
+    'ExtendedJournalHandler',
+    'openprocurement_client>=1.0b2'
+]
+
+api_requires = requires + [
+    'openprocurement.api',  # XXX TODO: set required version
+    'openprocurement.tender.core',
+]
+
+entry_points = {
+    'console_scripts': [
+        'contracting_data_bridge = openprocurement.contracting.api.databridge:main'
+    ],
+}
+
+setup(name='openprocurement.bridge.contracting',
+      version=version,
+      description="",
+      long_description=open("README.rst").read(),
+      classifiers=[
+        "Framework :: Pylons",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+        ],
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      url='https://github.com/openprocurement/openprocurement.bridge.contracting',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['openprocurement', 'openprocurement.bridge'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=test_requires,
+      extras_require={'api': api_requires, 'databridge': databridge_requires,
+                      'test': test_requires, 'docs': docs_requires},
+      entry_points=entry_points,
+      )
